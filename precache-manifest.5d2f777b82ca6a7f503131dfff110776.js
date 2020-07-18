d40f6abb4a093c914ffbf8ee34ad7ee219b86523
domi <dominika.hustinova@kiwi.com>
2020-07-18 13:30:22 +0200

    Updates

---
+++ precache-manifest.5d2f777b82ca6a7f503131dfff110776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7368cecb25b95ff41d56ff7c940453",
+    "url": "/react-quizz/index.html"
+  },
+  {
+    "revision": "aea74bb4beb174d9af18",
+    "url": "/react-quizz/static/js/2.7ed25286.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quizz/static/js/2.7ed25286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153229a98b7b942e6893",
+    "url": "/react-quizz/static/js/main.84bb2735.chunk.js"
+  },
+  {
+    "revision": "731a593a7ce3184e260a",
+    "url": "/react-quizz/static/js/runtime-main.93470ed0.js"
+  },
+  {
+    "revision": "8702cb14b01e052f95bf13a712a53671",
+    "url": "/react-quizz/static/media/andrzej-kryszpiniuk.8702cb14.jpg"
+  }
+]);
